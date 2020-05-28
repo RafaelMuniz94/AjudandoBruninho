d04ffa2e32a5494ae15b4c5cfacdf0c08da150de
Rafael Muniz <raruznh@gmail.com>
2020-05-27 22:35:10 -0300

    Adicionada questoes 10

--- Questao10.py
+++ Questao10.py
@@ -15,4 +15,42 @@
 
 #Formula = 200 + (0.09*vendasBrutas)
 
-
+l= [0, 0, 0, 0, 0, 0, 0, 0, 0]
+inter = ["1 - 200-299",
+         "2 - 300-399",
+         "3 - 400-499",
+         "4 - 500-599",
+         "5 - 600-699",
+         "6 - 700-799",
+         "7 - 800-899",
+         "8 - 900-999",
+         "9 - 1000 em diante"]
+x = 0
+while True:
+  n=float(input('Informe o salário %d° R$:'%(x+1)))
+  if 200 <= n <=299:
+    l[0]+=1
+  elif 300 <= n <=399:
+    l[1]+=1
+  elif 400 <= n <=499:
+    l[2]+=1
+  elif 500 <= n <=599:
+    l[3]+=1
+  elif 600 <= n <699:
+    l[4]+=1
+  elif 700 <= n <=799:
+    l[5]+=1
+  elif 800 <= n <=899:
+    l[6]+=1
+  elif 900 <= n <=999:
+    l[7]+=1
+  elif n>= 1000:
+     l[8]+=1
+  x+=1
+  d = input("Continuar o cálculo?(sim ou não)")
+  if d == 'n':
+      break
+print("\n")
+print("Informando a quantidade de cada salario:\n")
+for key,b in enumerate(l):
+    print(str(inter[key])+" : "+str(b))
--- Questao6.py
+++ Questao6.py
@@ -1,10 +1,8 @@
 # 6-Foram anotadas as idades e alturas de 30 alunos.
 #  Faça um Programa que determine quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
 
-IdadeAlunos = [12, 16, 11, 13, 14, 12, 11, 15, 14, 11, 15, 10, 14,
-               11, 14, 13, 13, 11, 14, 11, 16, 11, 11, 15, 13, 14, 15, 13, 16, 10]
-AlturaAlunos = [1.3, 1.8, 1.3, 1.5, 1.6, 1.5, 1.6, 1.8, 1.3, 1.5, 1.2, 1.2, 1.4, 1.3,
-                1.5, 1.2, 1.6, 1.5, 1.6, 1.8, 1.3, 1.5, 1.2, 1.2, 1.5, 1.6, 1.8, 1.3, 1.25, 1.33]
+IdadeAlunos = [12, 16, 11, 13, 14, 12, 11, 15, 14, 11, 15, 10, 14, 11, 14, 13, 13, 11, 14, 11, 16, 11, 11, 15, 13, 14, 15, 13, 16, 10]
+AlturaAlunos = [1.3, 1.8, 1.3, 1.5, 1.6, 1.5, 1.6, 1.8, 1.3, 1.5, 1.2, 1.2, 1.4, 1.3, 1.5, 1.2, 1.6, 1.5, 1.6, 1.8, 1.3, 1.5, 1.2, 1.2, 1.5, 1.6, 1.8, 1.3, 1.25, 1.33]
 
 
 def ObterMediaAlturas(vetorAltura):
